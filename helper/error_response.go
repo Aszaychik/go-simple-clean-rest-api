bc18045cafc38e53afa676ba0f67f1ec9491f5db
AsZaychik <aszaychik@gmail.com>
2023-10-04 15:41:25 +0700

    error response helper

---
+++ helper/error_response.go
@@ -0,0 +1,34 @@
+package helper
+
+import (
+	"net/http"
+	"unit_testing/model/web"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+func errorResponse(ctx echo.Context, code int, message string) error {
+	return ctx.JSON(code, web.WebResponse{
+		Code: code,
+		Message: message,
+		Data: nil,
+	})
+}
+
+func StatusNotFound(ctx echo.Context, err error) error {
+	return errorResponse(ctx, http.StatusNotFound, err.Error())
+}
+
+func StatusInternalServerError(ctx echo.Context, err error) error {
+	logrus.Error(err.Error())
+	return errorResponse(ctx, http.StatusInternalServerError, err.Error())
+}
+
+func StatusBadRequest(ctx echo.Context, err error) error {
+	return errorResponse(ctx, http.StatusBadRequest, err.Error())
+}
+
+func StatusEmailAlreadyExist(ctx echo.Context, err error) error {
+	return errorResponse(ctx, http.StatusConflict, err.Error())
+}
